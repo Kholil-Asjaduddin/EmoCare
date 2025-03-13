123305687165b4575a64ec0d6e3acf9e2a84a9ee
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-03-13 14:41:08 +0700

    chore: update .gitignore and remove Test.jsx from tracking

--- frontend/src/test/Test.jsx
+++ frontend/src/test/Test.jsx
@@ -4,6 +4,7 @@ const Test = () => {
     return (
         <div>
             <p className='text-sky-500'>Hello World!</p>
+            <p className='text-sky-500'>Hello World!</p>
         </div>
     )
 }
