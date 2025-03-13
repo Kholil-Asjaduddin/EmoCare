5efcc51da2555cdffd69e4a30ababe0967e8f845
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-03-13 16:44:34 +0700

    feat: add leave button for community page

--- frontend/src/components/CommunityPage/CommunityPage.jsx
+++ frontend/src/components/CommunityPage/CommunityPage.jsx
@@ -1,12 +1,14 @@
 import JoinButton from "./JoinButton";
 import ViewButton from "./ViewButton";
+import LeaveButton from "./LeaveButton";
 
 const CommunityPage = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-50">
             <JoinButton onClick={() => console.log("Join button clicked")} />
             <ViewButton onClick={() => console.log("View button clicked")} />
-        </div>           
+            <LeaveButton onClick={() => console.log("View button clicked")} />
+        </div>
     )
 }
 
