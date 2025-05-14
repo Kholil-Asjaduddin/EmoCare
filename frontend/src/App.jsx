a6a80def280d35b9e8197c6b0f1a528135504784
tsaniagaluh <tsaniagaluhbanggash@mail.ugm.ac.id>
2025-05-14 14:02:06 +0700

    update Navbar and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,36 +2,64 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./components/LandingPage/LandingPage";
 import SignupPage from "./components/SignupPage/SignupPage";
+import SelectRolePage from "./components/SelectRolePage/SelectRolePage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
+import HomePage from "./components/HomePage/HomePage";
+import ContentPage from "./components/ContentPage/ContentPage";
 import CommunityPage from "./components/CommunityPage/CommunityPage";
+import CommunityConvoPage from "./components/CommunityConvoPage/CommunityConvoPage";
 import ChatbotPage from "./components/ChatbotPage/ChatbotPage";
-import PsychologistPage from "./components/ConsultationPage/PsychologistPage";
+import ConsultationPage from "./components/ConsultationPage/PsychologistPage";
+import ConsultationChatPage from "./components/ConsultationChatPage/ConsultationChatPage";
+import PsychologistSessionPage from "./components/PsychologistSessionPage/PsychologistSessionPage";
+import PsychologistProfilePage from "./components/PsychologistProfilePage/PsychologistProfilePage";
+import UserProfilePage from "./components/UserProfilePage/UserProfilePage";
+
 import Navbar from "./components/Navbar";
 
 // Used for testing
 // import Test from "./test/Test";
 
+
+
 function App() {
   return (
     <Router>
       <Navbar />
-      <main className="pt-5">
+      <main className="flex flex-col items-center w-screen h-full">
         <Routes>
           {/* route to /landing */}
           <Route path="/landing" element={<LandingPage />} />
           {/* route to /signup */}
           <Route path="/signup" element={<SignupPage />} />
+          {/* route to /selectrole */}
+          <Route path="/selectrole" element={<SelectRolePage />} />
+          {/* route to /psychologistprofile */}
+          <Route path="/psychologistprofile" element={<PsychologistProfilePage />} />
+          {/* route to /userprofile */}
+          <Route path="/userprofile" element={<UserProfilePage />} />
           {/* route to /login */}
           <Route path="/login" element={<LoginPage />} />
           {/* route to /profile */}
           <Route path="/profile" element={<ProfilePage />} />
+          {/* route to /home */}
+          <Route path="/home" element={<HomePage />} />
+           {/* route to /content */}
+          <Route path="/content" element={<ContentPage />} />
           {/* route to /community */}
           <Route path="/community" element={<CommunityPage />} />
-          {/* route to /consultation */}
-          <Route path="/consultation" element={<PsychologistPage />} />
+
+          {/* route to /communityconvo */}
+          <Route path="/communityconvo" element={<CommunityConvoPage />} />
           {/* route to /chatbot */}
           <Route path="/chatbot" element={<ChatbotPage />} />
+          {/* route to /consultation */}
+          <Route path="/consultation" element={<ConsultationPage />} />
+          {/* route to /consultationchat */}
+          <Route path="/consultationchat" element={<ConsultationChatPage />} />
+          {/* route to /psychologistsession */}
+          <Route path="/psychologistsession" element={<PsychologistSessionPage />} />
 
           {/* route to /test */}
           {/* <Route path="/" element={<Test />} /> */}
