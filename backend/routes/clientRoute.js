96ebda08c6a8fd6cddefff214a330f45fa019433
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-05-26 23:20:35 +0700

    feat: add update client profile REST API

--- backend/routes/clientRoute.js
+++ backend/routes/clientRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { saveClientProfile } = require("../controllers/clientController");
+const { saveClientProfile, updateClientProfile } = require("../controllers/clientController");
 
 const router = express.Router();
 
 router.post("/save-profile", saveClientProfile);
+router.put("/update-profile", updateClientProfile);
 
 module.exports = router;
