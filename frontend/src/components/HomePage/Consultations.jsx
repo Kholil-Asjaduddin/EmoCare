50db5a9589e25d4425f65029dea6d444415abd5d
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-05-26 21:05:05 +0700

    feat: update dynamic booked consultation list on home page

--- frontend/src/components/HomePage/Consultations.jsx
+++ frontend/src/components/HomePage/Consultations.jsx
@@ -5,18 +5,18 @@ const Consultations = () => {
         <div className="flex flex-col items-center">
             <h3 className="text-2xl text-blue-light mb-5">Booked Consultation</h3>
 
-                <div className="w-3/4 flex flex-row gap-15 overflow-x-auto pb-6 scrollbar-hidden-hover">
+                <div className="w-3/4 flex flex-row gap-15 overflow-x-auto pb-6 justify-center scrollbar-hidden-hover">
                     <ConsultationCard
                         date="16 May 2025"
                         time="07:00 - 12:00"
                         name="Dr. Alice"
                     />
-                    {/* <ConsultationCard
+                    <ConsultationCard
                         date="5 Jan 2025"
                         time="10:00 - 12:00"
                         name="Abc, M.Psi"       
                     />
-                    <ConsultationCard
+                    {/* <ConsultationCard
                         date="10 Jan 2025"
                         time="09:00 - 10:30"
                         name="Def, S.Psi"
