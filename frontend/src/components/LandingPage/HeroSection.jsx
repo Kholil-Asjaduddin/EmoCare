a16020355b5b331ba2fb7de61f2dc92c1ba8272d
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-05-27 21:22:15 +0700

    feat: update profile menu

--- frontend/src/components/LandingPage/HeroSection.jsx
+++ frontend/src/components/LandingPage/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
             {/* Left Section (Text + Button) */}
             <div className="text-left px-50">
                 <h1 className="text-navy text-6xl font-extrabold">EmoCare</h1>
-                <Link to="/signup">
+                <Link to="/login">
                     <button className="mt-6 bg-teal text-blue-dark text-2xl font-medium px-8 py-4 rounded-full shadow-md
             hover:hover:shadow-2xl cursor-pointer transition-all duration-300">
                         Get Started
