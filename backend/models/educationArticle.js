69ead72530250712ae12e34348c31ee7ef97704d
tsaniagaluh <tsaniagaluhbanggash@mail.ugm.ac.id>
2025-05-29 23:41:45 +0700

    feat: add upload content education REST API

---
+++ backend/models/educationArticle.js
@@ -0,0 +1,8 @@
+class EducationArticle {
+    constructor(contentId, contentLink) {
+        this.contentId = contentId;
+        this.contentLink = contentLink;
+    }
+}
+
+module.exports = EducationArticle;
---
+++ backend/models/educationPodcast.js
@@ -0,0 +1,9 @@
+class EducationPodcast {
+    constructor(contentId, contentLink, title) {
+        this.contentId = contentId;
+        this.contentLink = contentLink;
+        this.title = title;
+    }
+}
+
+module.exports = EducationPodcast;
