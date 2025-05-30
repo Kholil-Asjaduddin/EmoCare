170793d74f9bd0fda80a99c554fc5bb8eb409c84
Kholil-Asjaduddin <kholilasjaduddin@mail.ugm.ac.id>
2025-05-30 07:43:57 +0700

    build: update firebase admin

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,6 +1,12 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("../serviceAccountKey.json");
+const admin = require("firebase-admin");
+const fs = require("fs");
 
+// Membuat file sementara untuk kredensial Firebase dari environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const filePath = "/tmp/serviceAccountKey.json";
+// fs.writeFileSync(filePath, JSON.stringify(serviceAccount));
+
+// Inisialisasi Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL
